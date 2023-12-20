72626bc3775846ea2f3e67d91cad246095e3b5b5
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-12-20 21:46:34 +0200

    fix upload file

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -2,41 +2,41 @@ import '@mantine/dropzone/styles.css';
 import { Group, Text, rem } from '@mantine/core';
 import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import Icon from './ui-assets/Icon';
+import Image from 'next/image';
 
-export function UploadFile(props: Partial<DropzoneProps>) {
+export function UploadFile({ handleUpload, fileUrls }: { handleUpload: any; fileUrls: [] }) {
   return (
     <Dropzone
-      onDrop={(files) => console.log('accepted files', files)}
+      onDrop={(files) => {
+        handleUpload(files)
+        console.log('accepted files', files)
+      }}
       onReject={(files) => console.log('rejected files', files)}
       maxSize={5 * 1024 ** 2}
       accept={IMAGE_MIME_TYPE}
-      {...props}
+      multiple
+      radius={24}
+      classNames={{root:"!bg-[#FDEED9]"}}
+
     >
       <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-        <Dropzone.Accept>
-            <Icon name='document-upload' size={52}  color={'var(--mantine-color-blue-6)'}/>
-        
-        </Dropzone.Accept>
-        <Dropzone.Reject>
-        <Icon name='Close' size={52}  color={'var(--mantine-color-red-6)'}/>
-        
-       
-        </Dropzone.Reject>
-        <Dropzone.Idle>
-        <Icon name='image' size={50}  color={'var(--mantine-color-dimmed)'}/>
-        
-        
-        </Dropzone.Idle>
 
-        <div>
-          <Text size="xl" inline>
-            Drag images here or click to select files
-          </Text>
-          <Text size="sm" c="dimmed" inline mt={7}>
-            Attach as many files as you like, each file should not exceed 5mb
-          </Text>
-        </div>
+
+        {fileUrls.length ? (fileUrls.map((img, index) => (
+          <div key={img?.name + index} className='relative w-20 h-20 '>
+            <Image src={ img.type == 'image/png' ||
+                    img.type == 'image/JPEG' ||
+                    img.type == 'image/jpg' ||
+                    img.type == 'image/jpeg' ||
+                    img.type == 'image/*'
+                      ? URL.createObjectURL(img):  '/default_upload_img.jpg'} fill alt={img?.name} className='object-contain object-center rounded-xl' /> </div>
+        ))) : (<Icon name='document-upload' size={50} color={'var(--mantine-color-dimmed)'} />)
+
+        }
       </Group>
+
+
+
     </Dropzone>
   );
 }
--- components/new-recipe/FirstRecipeDetailsStep1.tsx
+++ components/new-recipe/FirstRecipeDetailsStep1.tsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useState } from 'react';
-import { TextInput, Button, Textarea } from '@mantine/core';
-import { UploadFile } from '../UploadFile';
-
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
+import { TextInput, Button, Textarea } from "@mantine/core";
+import { UploadFile } from "../UploadFile";
 
 const validationSchema = Yup.object().shape({
-  recipeName: Yup.string().required('Required'),
-  recipeDescription: Yup.string().required('Required'),
-  recipeIngredients: Yup.array().of(Yup.string().required('Required')).min(1, 'Add at least one ingredient').required('Required'),
-  recipeImages: Yup.mixed().optional()
+  recipeName: Yup.string().required("Required"),
+  recipeDescription: Yup.string().required("Required"),
+  recipeIngredients: Yup.array()
+    .of(Yup.string().required("Required"))
+    .min(1, "Add at least one ingredient")
+    .required("Required"),
+  recipeImages: Yup.mixed().optional(),
 });
 
 function FirstRecipeDetailsStep1() {
   const [fileUrls, setFileUrls] = useState([]);
-
+  const [ingredients, setIngredients] = useState(["", "", ""]);
   const handleUpload = async (file: any) => {
     // Replace this with your actual upload function
-     const fileUrl = await uploadFile(file);
-     setFileUrls((prev) => [...prev, ...file]);
+    //  const fileUrl = await uploadFile(file);
+    setFileUrls(file);
     console.log(file);
-
   };
 
   return (
     <>
       <Formik
-        initialValues={{ recipeName: '', recipeDescription: '', recipeIngredients: ['', '', ''], recipeImages: null }}
+        initialValues={{
+          recipeName: "",
+          recipeDescription: "",
+          recipeIngredients: ingredients,
+          recipeImages: null,
+        }}
         validationSchema={validationSchema}
         onSubmit={(values, { setSubmitting }) => {
-          console.log('values', values)
+          console.log("values", values);
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
@@ -39,10 +45,12 @@ function FirstRecipeDetailsStep1() {
       >
         {({ isSubmitting, values }) => (
           <Form>
-            <div className='w-full grid lg:grid-cols-11 gap-x-28'>
-              <div className='col-span-6 grid gap-12'>
-                <h2 className='text-primary font-bold text-xl -mb-8'>First Recipe Details</h2>
-                <div className='space-y-2'>
+            <div className="w-full grid lg:grid-cols-11 gap-x-28 gap-6">
+              <div className="col-span-6 grid gap-12">
+                <h2 className="text-primary font-bold text-xl -mb-8">
+                  First Recipe Details
+                </h2>
+                <div className="space-y-2">
                   <TextInput
                     label="Recipe Name"
                     name="recipeName"
@@ -50,8 +58,12 @@ function FirstRecipeDetailsStep1() {
                     component={Field}
                     radius="xl"
                   />
-                  <ErrorMessage name="recipeName" render={(mess) => <p className='text-red-600'>{mess}</p>} />
-                </div><div className='space-y-2'>
+                  <ErrorMessage
+                    name="recipeName"
+                    render={(mess) => <p className="text-red-600">{mess}</p>}
+                  />
+                </div>
+                <div className="space-y-2">
                   <Textarea
                     radius="md"
                     autosize
@@ -60,41 +72,57 @@ function FirstRecipeDetailsStep1() {
                     label="Recipe Description"
                     name="recipeDescription"
                     placeholder="Enter Recipe Description"
-component={Field} className='h-24'
+                    component={Field}
+                    className="h-24"
                   />
-                  <ErrorMessage name="recipeDescription" render={(mess) => <p className='text-red-600'>{mess}</p>} />
+                  <ErrorMessage
+                    name="recipeDescription"
+                    render={(mess) => <p className="text-red-600">{mess}</p>}
+                  />
                 </div>
-                <div className='grid gap-6'>
-                  {[...Array(values.recipeIngredients.length).keys()].map((key) => (
-                    <div key={key} >
+                <div className="grid gap-6">
+                  {[...Array(ingredients.length).keys()].map((key) => (
+                    <div key={key}>
                       <TextInput
-
+                        radius="xl"
                         name={`recipeIngredients.${key}`}
                         placeholder={`Ingredient ${key + 1}`}
                         component={Field}
                       />
-                      <ErrorMessage name={`recipeIngredients.${key}`} render={(mess) => <p className='text-red-600'>{mess}</p>} />
+                      <ErrorMessage
+                        name={`recipeIngredients.${key}`}
+                        render={(mess) => (
+                          <p className="text-red-600">{mess}</p>
+                        )}
+                      />
                     </div>
                   ))}
-                </div></div>
-              <div className='col-span-5'>
-                <UploadFile onChange={handleUpload} />
-                <div>
-                  {fileUrls.map((url, index) => (
-                    <div key={index}>{url}</div>
-                  ))}
+                  <button
+                    onClick={() => {
+                      setIngredients((prev) => [...prev, ""]);
+                    }}
+                    className="bg-slate-300 w-[250px] text-center py-2 rounded-full"
+                  >
+                    Add new
+                  </button>
                 </div>
               </div>
+              <div className="col-span-5 space-y-4 ">
+                <h2 className="text-primary font-bold text-xl ">
+                  Recipe images
+                </h2>
 
+                <UploadFile handleUpload={handleUpload} fileUrls={fileUrls} />
+              </div>
             </div>
-            <Button type="submit" className='mt-6' disabled={isSubmitting}>
+            <Button type="submit" className="mt-6" disabled={isSubmitting}>
               Submit
             </Button>
           </Form>
         )}
       </Formik>
     </>
-  )
+  );
 }
 
-export default FirstRecipeDetailsStep1
+export default FirstRecipeDetailsStep1;
